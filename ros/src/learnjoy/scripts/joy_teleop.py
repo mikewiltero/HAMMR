79d22a5532e1fc8ca41d28c543f2417f62303c4e
mikewiltero <mike@kr-tech.net>
2016-07-17 17:34:54 -0400

    added function "translate" that is similar to arduino's map function. this will help maping the joy values to something for arduino

--- ros/src/learnjoy/scripts/joy_teleop.py
+++ ros/src/learnjoy/scripts/joy_teleop.py
@@ -13,7 +13,10 @@ def callback(data):
 	twist.angular.x = 160*data.axes[3] # move forward and backward
 	#sys.stdout.write("%f" %twist.angular.x)
 	# twist.angular.x is the thing that has the data for the x axis
-	pub.publish(twist.angular.x)
+	
+	# translate the raw joy value to a value friendly to the arduino servo function (value between 0-180
+	farts = translate(twist.angular.x, -160, 160, 0, 160)
+	pub.publish(farts)
 
 	
  
@@ -29,6 +32,25 @@ def joy_teleop():
  
 	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
+	
+# this function is similar to arduino's Map function.
+# "value" is the value needing to be translated
+# leftMin is the lower end of the incoming value
+# leftMax is the upper end of the incoming value
+# rightMin is the lower end of the translated value
+# rightMax is the upper end of the translated value
+# 
+# Source: http://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+	#figure out how 'wide' each range is
+	leftSpan = leftMax - leftMin
+	rightSpan = rightMax - rightMin
+	
+	#convert the left range into 0-1 range (float)
+	valueScaled = float(value - leftMin) / float(leftSpan)
+	
+	#convert the 0-1 range into a value in the right range.
+	return rightMin + (valueScaled * rightSpan)
  
 if __name__ == '__main__':
 	joy_teleop()
