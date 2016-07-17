6f42144be6601e7323c3905ad382f956ba9e88bb
mikewiltero <mike@kr-tech.net>
2016-07-17 17:06:48 -0400

    got this working with arduino. twist.angular.x is data

--- ros/src/learnjoy/scripts/joy_teleop.py
+++ ros/src/learnjoy/scripts/joy_teleop.py
@@ -3,25 +3,25 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 import sys
+from std_msgs.msg import UInt16
+import time
  
 def callback(data):
 	twist = Twist()
 	
 	# using the Twist type to get info from the joystick. the constant is a speed multiplier
-	twist.linear.x = 6*data.axes[] # move forward and backward
-	twist.linear.z = 6*data.axes[3] # yaw left/right
-	twist.linear.y = 6*data.axes[]
-	twist.angular.x = 6*data.axes[0]
-	twist.angular.y = 6*data.axes[1]
-	twist.angular.z = 6*data.axes[2]
-	pub.publish(twist)
+	twist.angular.x = 160*data.axes[3] # move forward and backward
+	#sys.stdout.write("%f" %twist.angular.x)
+	# twist.angular.x is the thing that has the data for the x axis
+	pub.publish(twist.angular.x)
+
 	
  
 def joy_teleop():
 	rospy.init_node('Joy_teleop')
 	rospy.Subscriber("joy", Joy, callback)
 	global pub
-	pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+	pub = rospy.Publisher('servo', UInt16, queue_size=10)
  
 	r = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
