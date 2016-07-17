3541ffd90e2e2707dea52df5bb5239a39ffe5edb
mikewiltero <mike@kr-tech.net>
2016-07-17 00:08:20 -0400

    joystick working, figured out things.

--- ros/src/learnjoy/scripts/joy_teleop.py
+++ ros/src/learnjoy/scripts/joy_teleop.py
@@ -2,12 +2,20 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+import sys
  
 def callback(data):
 	twist = Twist()
-	twist.linear.x = 4*data.axes[1]
-	twist.angular.z = 4*data.axes[2]
+	
+	# using the Twist type to get info from the joystick. the constant is a speed multiplier
+	twist.linear.x = 6*data.axes[] # move forward and backward
+	twist.linear.z = 6*data.axes[3] # yaw left/right
+	twist.linear.y = 6*data.axes[]
+	twist.angular.x = 6*data.axes[0]
+	twist.angular.y = 6*data.axes[1]
+	twist.angular.z = 6*data.axes[2]
 	pub.publish(twist)
+	
  
 def joy_teleop():
 	rospy.init_node('Joy_teleop')
